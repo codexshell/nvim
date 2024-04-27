97ef69d55c95ebe9c1717a5b64b6801e111d4408
Silvanos Eric <ericoderito@gmail.com>
2024-04-27 18:00:07 +0300

    chore(vimrc): set scrolloff opt to 5

--- lua/options.lua
+++ lua/options.lua
@@ -5,6 +5,11 @@ require "nvchad.options"
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
 
+-- vim start
+vim.opt.scrolloff = 5
+-- vim end
+
+
 -- codeium start
 vim.g.codeium_enabled = false
 -- codeium end
